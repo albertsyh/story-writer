4d4da874a9bb75b6710684e94ac2d7db9f114ac6
Albert Shim <albert@synomus.io>
2021-11-05 16:08:02 +0800

    created word resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StoriesModule } from './stories/stories.module';
+import { WordModule } from './word/word.module';
 
 @Module({
-  imports: [StoriesModule],
+  imports: [StoriesModule, WordModule],
   controllers: [AppController],
   providers: [AppService],
 })
